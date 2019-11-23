de3c70f520bc3670e94e031cae553fbac82f319e
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2019-11-23 08:53:39 +0100

    split the example code in two apss

---
+++ examples/Counter2.purs
@@ -0,0 +1,69 @@
+module Example.Counter2 where
+import Prelude
+import Data.Int (even)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Effect (Effect)
+import Run (match)
+import Pha (VDom, Event, app, text)
+import Pha.Action (Action, setState, DELAY, delay, delayEffect)
+import Pha.Event (key) as E
+import Pha.Html (div', button, span, click, class')
+
+type State = {
+    counter :: Int
+}
+
+-- effects used in this app
+type EFFS = (delay :: DELAY)
+
+-- initial state
+state :: State
+state = {
+    counter: 0
+}
+
+-- pure actions
+increment :: forall effs. Action State effs
+increment = setState \{counter} -> {counter: counter + 1}
+
+-- actions with effects
+delayedIncrement :: forall effs. Action State (delay :: DELAY | effs)
+delayedIncrement = delay 1000 *> increment
+
+view :: State -> VDom State EFFS
+view {counter} = 
+    div' [] [
+        div' [class' "counter" true] [text $ show counter],
+        button [click increment] [text "Increment"],
+        button [click delayedIncrement] [text "Delayed Increment"],
+
+        div' [] [
+            span [] [text "green when the counter is even"],
+            div' [
+                class' "box" true, 
+                class' "even" (even counter)
+            ] []
+        ],
+
+        div' [] [
+            text "press space to increment the counter"
+        ]
+    ]
+
+onKeydown :: forall effs. Event -> Action State effs
+onKeydown ev = case E.key (ev) of
+    Just " " -> increment
+    _ -> pure unit
+
+main :: Effect Unit
+main = app {
+    state,           -- initial state
+    view,            -- a mapping of the state to virtual dom
+    init: pure unit, -- action triggered at the start of the app (no action here)
+    node: "root",    -- the id of the root node of the app
+    events: [Tuple "keydown" onKeydown],
+    effects: match {
+        delay: delayEffect
+    }
+}
---
+++ examples/Random.purs
@@ -0,0 +1,66 @@
+module Example.Random where
+import Prelude
+import Data.Int (toNumber)
+import Data.Array ((..), mapWithIndex)
+import Effect (Effect)
+import Run (match)
+import Pha (VDom, Event, app, text)
+import Pha.Action (Action, getState, setState,
+                    RNG, rngEffect)
+import Pha.Random (randomInt, shuffle)
+import Pha.Html (div', button, click, class', style, pc)
+
+type State = {
+    dice :: Int,
+    puzzle :: Array Int
+}
+
+-- effects used in this app
+type EFFS = (rng :: RNG)
+
+-- initial effects
+state :: State
+state = {
+    dice: 1,
+    puzzle: 0 .. 15
+}
+
+rollDice :: forall effs. Action State (rng :: RNG | effs)
+rollDice = do
+    rolled <- randomInt 6 <#> (_ + 1)
+    setState _{dice = rolled}
+
+shufflePuzzle :: forall effs. Action State (rng :: RNG | effs)
+shufflePuzzle = do
+    {puzzle} <- getState
+    shuffled <- shuffle puzzle
+    setState _{puzzle = shuffled}
+
+view :: State -> VDom State EFFS
+view {dice, puzzle} = 
+    div' [] [
+        div' [class' "counter" true] [text $ show dice],
+        button [click rollDice] [text "Roll dice"],
+
+        div' [class' "puzzle" true] (
+            puzzle # mapWithIndex \i j ->
+                div' [
+                    class' "puzzle-item" true,
+                    style "left" $ pc (0.25 * toNumber (j / 4)),
+                    style "top" $ pc (0.25 * toNumber (j `mod` 4)) 
+                ] [text $ show i]
+        ),
+        button [click shufflePuzzle] [text "Shuffle"]
+    ]
+
+main :: Effect Unit
+main = app {
+    state,
+    view,
+    init: rollDice,
+    node: "root",
+    events: [],
+    effects: match {
+        rng: rngEffect
+    }
+}

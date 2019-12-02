1be885a97361181308bccd2f201c913d61abe347
Guillaume Bagan <guillaume.bagn@liris.cnrs.fr>
2019-12-02 10:43:08 +0100

    typo

--- src/pha/Events.purs
+++ src/pha/Events.purs
@@ -3,7 +3,7 @@ module Pha.Events (onclick, onclick',
     onpointerup, onpointerup', onpointerdown, onpointerdown', onpointerenter, onpointerenter', onpointerleave, onpointerleave',
     oncontextmenu, oncontextmenu',
     onvaluechange, onchecked,
-    on, on', custom, preventDefaultOn, stopPropagationOn, releasePointerCaptureE) where
+    on, on', custom, preventDefaultOn, stopPropagationOn, releasePointerCaptureOn) where
 
 import Prelude hiding (div)
 import Effect (Effect)

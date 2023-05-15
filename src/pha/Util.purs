6e7d74e8b88ac4ec16e70ac29134eb997bab1992
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-05-15 15:51:46 +0200

    memoized -> memoize

--- src/pha/Util.purs
+++ src/pha/Util.purs
@@ -1,13 +1,18 @@
-module Pha.Util (memoized, memoCompose, memoCompose') where
+module Pha.Util (memoize, memoCompose, memoCompose') where
 
 import Prelude
 
 foreign import memoizedImpl :: forall a b c. (a -> b) -> (b -> c) -> a -> c
 foreign import memoizedObj :: forall a b c. (a -> b) -> (b -> c) -> a -> c
 
-memoized :: forall a b. (a -> b) -> a -> b
-memoized = memoizedImpl identity
+-- | Memoize the function f.
+-- | If the argument of f differs from the previous call, then f is recomputed.
+memoize :: forall a b. (a -> b) -> a -> b
+memoize = memoizedImpl identity
 
+-- | Memoize the composition of two functions f and g
+-- | If the argument of f equals from the previous call, then f is recomputed.
+-- | If the argument of g differs from the previous call, then g is recomputed.
 memoCompose :: forall a b c. (a -> b) -> (b -> c) -> a -> c
 memoCompose = memoizedImpl
 

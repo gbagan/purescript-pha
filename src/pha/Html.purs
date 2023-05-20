e59cec11f62cfa563687ca4fe4f4c36979973a93
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-05-20 10:18:27 +0200

    export clipPath and defs

--- src/pha/Html.purs
+++ src/pha/Html.purs
@@ -1,4 +1,4 @@
 module Pha.Html (module Export) where
 
-import Pha.Html.Elements (a, br, button, circle, div, ellipse, footer, form, g, h1, h2, h3, h4, h5, h6, header, hr, img, image, input, label, li, line, main, nav, ol, option, p, path, rect, section, select, span, svg, text_, textarea, ul, use) as Export
+import Pha.Html.Elements (a, br, button, circle, clipPath, defs, div, ellipse, footer, form, g, h1, h2, h3, h4, h5, h6, header, hr, img, image, input, label, li, line, main, nav, ol, option, p, path, rect, section, select, span, svg, text_, textarea, ul, use) as Export
 import Pha.Html.Core (Event, EventHandler, Prop, Html, attr, class', class_, empty, fromMaybe, elem, keyed, lazy, lazy2, lazy3, lazy4, lazy5, maybe, on_, prop, style, text, when) as Export
